0827ad6a5963b184576b82f209884702b29515e6
maniacbug <maniacbug@ymail.com>
2011-09-18 11:41:35 -0700

    Test too many objects

--- Updater.h
+++ Updater.h
@@ -14,8 +14,9 @@ class IUpdate;
 
 class Updater
 {
-private:
+public:
   static const int max_objects = 10; /**< Max # of objects we can handle */
+private:
   IUpdate* objects[max_objects]; /**< Set of known objects */
   IUpdate** end_objects; /**< Pointer just past the last known object */
 protected:
