5046bbb98000693873331de04aa383e0161854b1
maniacbug <maniacbug@ymail.com>
2011-09-18 11:22:17 -0700

    Added Updater to manage IUpdate objects

---
+++ Updater.h
@@ -0,0 +1,27 @@
+#ifndef __UPDATER_H__
+#define __UPDATER_H__
+
+// STL includes
+// C includes
+// Library includes
+// Project includes
+
+/**
+ * Manages all IUpdate objects
+ */
+
+class Updater
+{
+private:
+protected:
+public:
+  /**
+   * Determine the number of objects currently known t the system
+   *
+   * @return Number of objects being updated
+   */
+  size_t size(void) const { return 0; }
+};
+
+#endif // __UPDATER_H__
+// vim:cin:ai:sts=2 sw=2 ft=cpp
