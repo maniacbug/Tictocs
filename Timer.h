33fcb2d1963c7141043cace665000bfc14e73cc4
maniacbug <maniacbug@ymail.com>
2011-09-18 12:51:24 -0700

    Created wasFired

--- Timer.h
+++ Timer.h
@@ -5,16 +5,37 @@
 // C includes
 // Library includes
 // Project includes
+#include <IUpdate.h>
 
 /**
  * Manage events which occur on a specified time interval
  */
 
-class Timer
+class Timer: public IUpdate
 {
 private:
+  unsigned long interval; /**< How long to wait before firing */
+  unsigned long last_updated;
+  bool fired;
 protected:
+  /**
+   * Call regularly to update status of timer
+   */
+   void update(void) {}
 public:
+  /**
+   * Constructor
+   *
+   * @param _interval How long to wait before firing
+   */
+  Timer(unsigned long _interval): interval(_interval), last_updated(0), fired(false) {}
+
+  /**
+   * Test whether the timer has fired since we last asked
+   *
+   * @retval true Timer fired since this method was last called
+   */
+  bool wasFired(void) { bool result = fired; fired = false; return result; }
 };
 
 #endif // __TIMER_H__
