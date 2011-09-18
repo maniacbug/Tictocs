6a4a25e038f2bd9e5a70e05ed775f9643cf9eaec
maniacbug <maniacbug@ymail.com>
2011-09-18 11:14:51 -0700

    created iupdate

---
+++ IUpdate.h
@@ -0,0 +1,22 @@
+#ifndef __IUPDATE_H__
+#define __IUPDATE_H__
+
+// STL includes
+// C includes
+// Library includes
+// Project includes
+
+/**
+ * Example for how classes should be declared
+ */
+
+class IUpdate
+{
+private:
+protected:
+public:
+  virtual void update(void) = 0;
+};
+
+#endif // __IUPDATE_H__
+// vim:cin:ai:sts=2 sw=2 ft=cpp
