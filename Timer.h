e2c262aff05e10690913ff2ef0a56726d26a0430
maniacbug <maniacbug@ymail.com>
2011-09-18 12:43:04 -0700

    Created timer class

---
+++ Timer.h
@@ -0,0 +1,21 @@
+#ifndef __TIMER_H__
+#define __TIMER_H__
+
+// STL includes
+// C includes
+// Library includes
+// Project includes
+
+/**
+ * Manage events which occur on a specified time interval
+ */
+
+class Timer
+{
+private:
+protected:
+public:
+};
+
+#endif // __TIMER_H__
+// vim:cin:ai:sts=2 sw=2 ft=cpp
