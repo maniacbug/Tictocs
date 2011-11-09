9dc4afac956e6f768e668ce83ec3b41d7a17a16e
Ace Jones <ace.jones1@yahoo.com>
2011-11-09 06:54:18 -0800

    PinControl working

--- PinControl.cpp
+++ PinControl.cpp
@@ -12,4 +12,30 @@
 
 /****************************************************************************/
 
+void PinControl::begin(void)
+{
+  pinMode(pin,OUTPUT);
+  digitalWrite(pin,LOW);
+}
+
+/****************************************************************************/
+
+void PinControl::listen(Connectable* _whom)
+{
+  Connectable::listen(_whom,signal_low);
+  Connectable::listen(_whom,signal_high);
+}
+
+/****************************************************************************/
+
+void PinControl::onNotify(const Connectable*, uint8_t signal)
+{
+  if (signal == signal_low)
+    digitalWrite(pin,LOW);
+  else if (signal == signal_high)
+    digitalWrite(pin,HIGH);
+}
+
+/****************************************************************************/
+
 // vim:cin:ai:sts=2 sw=2 ft=cpp
--- PinControl.h
+++ PinControl.h
@@ -4,17 +4,25 @@
 // STL includes
 // C includes
 // Library includes
+#include <Connector.h>
 // Project includes
 
 /**
  * Example for how classes should be declared
  */
 
-class PinControl
+class PinControl: public Connectable
 {
 private:
+  int pin;
+  uint8_t signal_low;
+  uint8_t signal_high;
 protected:
+  void onNotify(const Connectable*, uint8_t signal);
 public:
+  PinControl(Connector& _conn,int _pin,uint8_t _signal_low, uint8_t _signal_high): Connectable(_conn), pin(_pin), signal_low(_signal_low), signal_high(_signal_high) {}
+  void begin(void);
+  void listen(Connectable* _whom);
 };
 
 #endif // __PINCONTROL_H__
