9c0174a419864a5e5051d57af336ac96b776b8f5
Ace Jones <ace.jones1@yahoo.com>
2011-11-09 06:33:46 -0800

    Templates for PinControl class

---
+++ PinControl.cpp
@@ -0,0 +1,15 @@
+// STL includes
+// C includes
+// Library includes
+#if ARDUINO >= 100
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+// Project includes
+#include <PinControl.h>
+
+/****************************************************************************/
+
+// vim:cin:ai:sts=2 sw=2 ft=cpp
---
+++ PinControl.h
@@ -0,0 +1,21 @@
+#ifndef __PINCONTROL_H__
+#define __PINCONTROL_H__
+
+// STL includes
+// C includes
+// Library includes
+// Project includes
+
+/**
+ * Example for how classes should be declared
+ */
+
+class PinControl
+{
+private:
+protected:
+public:
+};
+
+#endif // __PINCONTROL_H__
+// vim:cin:ai:sts=2 sw=2 ft=cpp
