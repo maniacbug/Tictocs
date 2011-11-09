a11dbf5bc7deade8d679e9fd91aa1b0bce53886d
Ace Jones <ace.jones1@yahoo.com>
2011-11-08 22:00:59 -0800

    Made EmitButton actually connectable.  Wrote the whole test to bring up an emit button

--- EmitButton.h
+++ EmitButton.h
@@ -6,12 +6,13 @@
 // Library includes
 // Project includes
 #include <Button.h>
+#include <Connector.h>
 
 /**
  * Manages a single push button which emits signals on press and release
  */
 
-class EmitButton: public Button
+class EmitButton: public Connectable, public Button
 {
 public:
   /**
@@ -19,7 +20,7 @@ public:
    *
    * @param _pin Which pin the button is monitoring
    */
-  EmitButton(int _pin, unsigned long _interval, int /*_signal_pressed*/, int /*_signal_released*/): Button(_pin,_interval) {}
+  EmitButton(Connector& _conn,int _pin, unsigned long _interval, int /*_signal_pressed*/, int /*_signal_released*/ =0): Connectable(_conn), Button(_pin,_interval) {}
 };
 #endif // __EMITBUTTON_H__
 // vim:cin:ai:sts=2 sw=2 ft=cpp
