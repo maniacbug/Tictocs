1dde0c76d2ec7d0cc6c32b8974c4f8c4e6b9e219
Ace Jones <ace.jones1@yahoo.com>
2011-11-09 10:50:42 -0800

    Stubs for RtcEvent class

---
+++ RtcEvent.cpp
@@ -0,0 +1,9 @@
+// STL includes
+// C includes
+// Library includes
+// Project includes
+#include <RtcEvent.h>
+
+/****************************************************************************/
+
+// vim:cin:ai:sts=2 sw=2 ft=cpp
---
+++ RtcEvent.h
@@ -0,0 +1,21 @@
+#ifndef __RTCEVENT_H__
+#define __RTCEVENT_H__
+
+// STL includes
+// C includes
+// Library includes
+// Project includes
+
+/**
+ * Example for how classes should be declared
+ */
+
+class RtcEvent
+{
+private:
+protected:
+public:
+};
+
+#endif // __RTCEVENT_H__
+// vim:cin:ai:sts=2 sw=2 ft=cpp
