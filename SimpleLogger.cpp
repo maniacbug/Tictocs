b2fea00e400dc3d22c3580dc7d2df73846e28412
maniacbug <maniacbug@ymail.com>
2011-11-13 19:04:57 -0800

    Stub files for SimpleLogger

---
+++ SimpleLogger.cpp
@@ -0,0 +1,17 @@
+/*
+ Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
+// STL includes
+// C includes
+// Library includes
+// Project includes
+#include <SimpleLogger.h>
+
+/****************************************************************************/
+
+// vim:cin:ai:sts=2 sw=2 ft=cpp
---
+++ SimpleLogger.h
@@ -0,0 +1,29 @@
+/*
+ Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
+#ifndef __SIMPLELOGGER_H__
+#define __SIMPLELOGGER_H__
+
+// STL includes
+// C includes
+// Library includes
+// Project includes
+
+/**
+ * Example for how classes should be declared
+ */
+
+class SimpleLogger
+{
+private:
+protected:
+public:
+};
+
+#endif // __SIMPLELOGGER_H__
+// vim:cin:ai:sts=2 sw=2 ft=cpp
