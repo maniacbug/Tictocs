02fc9606b0d4b2cafbb6867fc9fb988bb38a3968
maniacbug <maniacbug@ymail.com>
2011-11-13 21:26:01 -0800

    Add object symbols to SimpleLogger, and fix a few bugs in signal symbols

--- SimpleLogger.cpp
+++ SimpleLogger.cpp
@@ -19,6 +19,7 @@ SimpleLogger::SimpleLogger(int _num_objects, int _num_signals):
   next_signal(0)
 {
   signal_dictionary = reinterpret_cast<signal_entry*>(malloc(_num_signals * sizeof(signal_entry)));
+  object_dictionary = reinterpret_cast<object_entry*>(malloc(_num_objects * sizeof(object_entry)));
 }
 
 /****************************************************************************/
@@ -27,7 +28,7 @@ prog_char* SimpleLogger::find_symbol(uint8_t signal)
 {
   prog_char* result = NULL;
 
-  int i = num_signals;
+  int i = next_signal;
   while(i--)
   {
     signal_entry* current = signal_dictionary + i;
@@ -40,20 +41,57 @@ prog_char* SimpleLogger::find_symbol(uint8_t signal)
 
 /****************************************************************************/
 
-void SimpleLogger::log_emit(const Connectable* p, uint8_t signal)
+prog_char* SimpleLogger::find_symbol(const Connectable* object)
 {
-  prog_char* signal_symbol = find_symbol(signal);
-  if ( signal_symbol )
-    printf_P(PSTR("EMIT %p %S\n\r"),p,signal_symbol);
-  else
-    printf_P(PSTR("EMIT %p %i\n\r"),p,signal);
+  prog_char* result = NULL;
+
+  int i = next_object;
+  while(i--)
+  {
+    object_entry* current = object_dictionary + i;
+    if ( current->object == object )
+      result = current->symbol; 
+  }
+
+  return result;
 }
 
 /****************************************************************************/
 
-void SimpleLogger::log_notify(const Connectable* p)
+void SimpleLogger::log_emit(const Connectable* object, uint8_t signal)
 {
-  printf_P(PSTR("NOTF %p\n\r"),p);
+  prog_char* signal_symbol = find_symbol(signal);
+  prog_char* object_symbol = find_symbol(object);
+
+  int state = (signal_symbol?1:0) + (object_symbol?2:0);
+
+  switch (state)
+  {
+  case 0:
+    printf_P(PSTR("EMIT %p %i\n\r"),object,signal);
+    break;
+  case 1:
+    printf_P(PSTR("EMIT %p %S\n\r"),object,signal_symbol);
+    break;
+  case 2:
+    printf_P(PSTR("EMIT %S %p\n\r"),object_symbol,signal);
+    break;
+  case 3:
+    printf_P(PSTR("EMIT %S %S\n\r"),object_symbol,signal_symbol);
+    break;
+  }
+}
+
+/****************************************************************************/
+
+void SimpleLogger::log_notify(const Connectable* object)
+{
+  prog_char* object_symbol = find_symbol(object);
+  
+  if ( object_symbol )
+    printf_P(PSTR("NOTF %S\n\r"),object_symbol);
+  else
+    printf_P(PSTR("NOTF %p\n\r"),object);
 }
 
 /****************************************************************************/
@@ -67,6 +105,23 @@ bool SimpleLogger::setSymbol(uint8_t signal, prog_char* symbol)
     result = true;
     signal_dictionary[next_signal].symbol = symbol;
     signal_dictionary[next_signal].signal = signal;
+    next_signal++;
+  }
+  return result;
+}
+
+/****************************************************************************/
+
+bool SimpleLogger::setSymbol(const Connectable* object, prog_char* symbol)
+{
+  bool result = false;
+
+  if ( next_object < num_objects )
+  {
+    result = true;
+    object_dictionary[next_object].symbol = symbol;
+    object_dictionary[next_object].object = object;
+    next_object++;
   }
   return result;
 }
--- SimpleLogger.h
+++ SimpleLogger.h
@@ -31,6 +31,11 @@ protected:
     uint8_t signal;
     prog_char* symbol;
   };
+  struct object_entry
+  {
+    const Connectable* object;
+    prog_char* symbol;
+  };
 private:
   int num_objects;
   int next_object;
@@ -38,15 +43,18 @@ private:
   int next_signal;
 
   signal_entry* signal_dictionary;
+  object_entry* object_dictionary;
 protected:
   virtual void log_emit(const Connectable*, uint8_t);
   virtual void log_notify(const Connectable*);
   prog_char* find_symbol(uint8_t signal);
+  prog_char* find_symbol(const Connectable* object);
 public:
   // Send in number of objects & signals to make room for in the
   // dictionary
   SimpleLogger(int _num_objects, int _num_signals);
   bool setSymbol(uint8_t, prog_char*);
+  bool setSymbol(const Connectable*, prog_char*);
 };
 
 #endif // __SIMPLELOGGER_H__
