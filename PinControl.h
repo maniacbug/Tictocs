512bf5326967bbeeb9d33544fb67fafde9199d57
maniacbug <maniacbug@ymail.com>
2012-02-18 22:24:40 -0800

    Add method to access the pin

--- PinControl.h
+++ PinControl.h
@@ -19,6 +19,7 @@ private:
   uint8_t signal_high;
 protected:
   void onNotify(const Connectable*, uint8_t signal);
+  uint8_t get_pin(void) const { return pin; }
 public:
   PinControl(Connector& _conn,int _pin,uint8_t _signal_low, uint8_t _signal_high): Connectable(_conn), pin(_pin), signal_low(_signal_low), signal_high(_signal_high) {}
   void begin(void);
