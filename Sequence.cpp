b05fd392e78e67f91e38c2803099a6bef4731fd1
maniacbug <maniacbug@ymail.com>
2012-05-01 20:49:12 -0700

    Added sequence

---
+++ Sequence.cpp
@@ -0,0 +1,74 @@
+/*
+ Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
+// STL headers
+// C headers
+// Framework headers
+// Library headers
+// Project headers
+// This component's header
+#include <Sequence.h>
+
+/****************************************************************************/
+
+Sequence::Sequence(Connector& conn,const Entry* _entries,uint8_t _signal_launch): Connectable(conn), Tictocs::Timer(0), entries(_entries), current(_entries), signal_launch(_signal_launch)
+{
+  disable();
+}
+
+/****************************************************************************/
+
+void Sequence::onFired(void)
+{
+  disable();
+  if ( isValid() ) 
+  {
+    emit( pgm_read_byte(&(current->signal)) );
+    ++current;
+    resetInterval();
+  }
+}
+
+/****************************************************************************/
+
+void Sequence::onNotify(const Connectable* /*who*/, uint8_t signal)
+{
+  if ( signal == signal_launch )
+  {
+    // Restart!
+    current = entries;
+    resetInterval();
+  }
+}
+
+/****************************************************************************/
+
+bool Sequence::isValid(void) const
+{
+  return current && pgm_read_byte(&(current->signal));
+}
+
+/****************************************************************************/
+
+void Sequence::resetInterval(void)
+{
+  if ( isValid() )
+  {
+    setInterval(pgm_read_dword(&(current->delayms)));
+  }
+}
+
+/****************************************************************************/
+void Sequence::begin(Updater& updater,Connectable* whom)
+{
+  Tictocs::Timer::begin(updater);
+  listen(whom,signal_launch);
+}
+/****************************************************************************/
+
+// vim:cin:ai:sts=2 sw=2 ft=cpp
---
+++ Sequence.h
@@ -0,0 +1,92 @@
+/*
+ Copyright (C) 2012 J. Coliz <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
+#ifndef __SEQUENCE_H__
+#define __SEQUENCE_H__
+
+// STL headers
+// C headers
+// Framework headers
+// Library headers
+#include <TictocTimer.h>
+// Project headers
+
+/**
+ * A sequence of signals which will be fired according to a timeline.
+ *
+ * It is a connectable timer that contains an array of delay/signal pairs.
+ * The basic idea is to wait the delay for each, and then fire the signal.
+ * Also, the sequence wakes up upon hearing its own wake-up signal.  It's
+ * dormant until that.
+ */
+
+class Sequence: public Connectable, public Tictocs::Timer 
+{
+public:
+  /**
+   * Static structure which can be used to construct a sequence
+   */
+  struct Entry
+  {
+    uint32_t delayms; /**< MS to wait before emitting */
+    uint8_t signal; /**< Signal to emit */
+  };
+private:
+  const Entry* entries; /**< Current list of static entries, in flash */
+  const Entry* current; /**< Current entry we're waiting for */
+  uint8_t signal_launch; /**< Signal we listen for */
+protected:
+  /**
+   * Event triggered when timer fires
+   */
+  virtual void onFired(void);
+  
+  /**
+   * Event triggered when receiving a signal we're listening for
+   *
+   * @param who Object which emitted the signal
+   * @param signal Signal which was emitted
+   */
+  virtual void onNotify(const Connectable* who, uint8_t signal);
+
+  /**
+   * Whether this sequence has a valid current entry
+   *
+   * @return Whether the sequence has a valid current entry 
+   */
+  bool isValid(void) const;
+
+  /**
+   * Reset the interval and restart the timer, using the interval for the
+   * current entry.
+   */
+  void resetInterval(void);
+public:
+  /**
+   * Fully-qualified constructor
+   *
+   * @param conn Connector used to emit our signals
+   * @param entries Array of entries, in flash memory.  Terminated with a {0,0}.
+   * @param _signal_launch Signal which will start the sequence 
+   */
+  Sequence(Connector& conn,const Entry*,uint8_t _signal_launch); 
+
+  /**
+   * Begin operation
+   *
+   * Call this before the sequence will fully function
+   *
+   * @param updater Updater which will give this a time slice
+   * @param whom Object to listen to, or NULL for listen to all
+   */
+  void begin(Updater& updater,Connectable* whom = NULL);
+
+};
+
+#endif // __SEQUENCE_H__
+// vim:cin:ai:sts=2 sw=2 ft=cpp
